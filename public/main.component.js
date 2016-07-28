13d314e3c4980a72695f5389966de4bdc0367bc7
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-07-28 18:20:01 +0800

    first iteration

--- public/main.component.js
+++ public/main.component.js
@@ -1,7 +1,18 @@
 angular.module('chatApp')
-  .component('mainChat', {
-    templateUrl: 'main.template.html',
-    controller: function () {
-      
+.component('mainChat', {
+  templateUrl: 'main.template.html',
+  controller: function MainController ($scope, socket) {
+    var ctrl = this
+    this.msgs = []
+    this.sendMsg = function () {
+      console.log(this)
+      event.preventDefault()
+      socket.emit('send msg', this)
+      this.text = ''
     }
-  })
+    socket.on('get msg', function (data) {
+      ctrl.msgs.push(data)
+      $scope.$digest()
+    })
+  }
+})
--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const app = express()
 const port = process.env.PORT || 3000
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const connections = [];
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/bower_components'))
 
 io.on('connection', (socket) => {
   console.log('a user has connected')
+  //new connection,
   socket.on('disconnect', () => {
     console.log('a user has disconnected')
   })
   socket.on('send msg', function (data) {
+    console.log(data)
+    // console.log(data.userName, data.msgText)
     io.sockets.emit('get msg', data)
   })
 })
